799617108121b2d83e039ef1f76dc25b3c417d3e
kent <kentvan21@gmail.com>
2023-07-31 22:39:36 -0500

    Created a navbar with login and registration

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,27 +3,33 @@ import { useEffect, useState } from "react";
 import React from 'react'
 
 const Product = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("/products")
+    .then((response) => response.json())
+    .then((data) => setProducts(data))
+    .catch((error) => console.error('Error fetching product:', error));
+  }, []);
 
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        fetch("/products")
-        .then((response) => response.json())
-        .then((data) => setProducts(data))
-        .catch((error) => console.error('Error fetching product:', error));
-    }, []);
-
-    return (
-        <div>
-            <h3>Product List</h3>
-            <ul>
-                {products.map((product) => (
-                    <li>
-                        <p>{product.name} - ${product.price}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Product Name</th>
+            <th scope="col">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr>
+              <th scope="row">{product.name}</th>
+              <td>${product.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
     )
 }
 
