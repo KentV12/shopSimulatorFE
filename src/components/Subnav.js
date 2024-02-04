221e3edc32b640044bcd206b7bc4c66033c843b8
kent <kentvan21@gmail.com>
2024-02-03 19:01:34 -0600

    styling for category dropdown

--- src/components/Subnav.js
+++ src/components/Subnav.js
@@ -3,23 +3,75 @@ import { Dropdown } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 const Subnav = () => {
   return (
     <Navbar expand="lg" className="my-3">
-      <Navbar.Brand href="#home">
-        <Dropdown>
-          <Dropdown.Toggle variant="success" id="dropdown-basic" size="lg">
-            All Departments
-          </Dropdown.Toggle>
+      {/* <Navbar.Brand href="#home" className="w-100"> */}
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic" size="lg">
+          <span className="me-2">
+            <img src={require("../images/list.svg").default} alt="list" />
+          </span>
+          <strong>All Categories</strong>
+        </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Drinks and Snacks</Dropdown.Item>
+        <Dropdown.Menu>
+          <Container fluid>
+            <Row>
+              <Col>
+                <Dropdown.Item className="text-newgreen" href="#/action-1">
+                  Drinks and Snacks
+                </Dropdown.Item>
+                <Dropdown.Item href="#/action-1">
+                  Item
+                </Dropdown.Item>
+                <Dropdown.Item href="#/action-1">
+                  Item
+                </Dropdown.Item>
+                <Dropdown.Item href="#/action-1">
+                  Item
+                </Dropdown.Item>
+              </Col>
+              <Col>
+                <Dropdown.Item className="text-newgreen" href="#/action-2">
+                  Dairy and Eggs
+                </Dropdown.Item>
+                <Dropdown.Item href="#/action-1">
+                  Item
+                </Dropdown.Item>
+                <Dropdown.Item href="#/action-1">
+                  Item
+                </Dropdown.Item>
+                <Dropdown.Item href="#/action-1">
+                  Item
+                </Dropdown.Item>
+              </Col>
+              <Col>
+                <Dropdown.Item className="text-newgreen" href="#/action-3">
+                  Chicken, Meat and Fish
+                </Dropdown.Item>
+                <Dropdown.Item href="#/action-1">
+                  Item
+                </Dropdown.Item>
+                <Dropdown.Item href="#/action-1">
+                  Item
+                </Dropdown.Item>
+                <Dropdown.Item href="#/action-1">
+                  Item
+                </Dropdown.Item>
+              </Col>
+            </Row>
+          </Container>
+          {/* <Dropdown.Item href="#/action-1">Drinks and Snacks</Dropdown.Item>
             <Dropdown.Item href="#/action-2">Dairy and Eggs</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Chicken, Meat and Fish</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Navbar.Brand>
+            <Dropdown.Item href="#/action-3">Chicken, Meat and Fish</Dropdown.Item> */}
+        </Dropdown.Menu>
+      </Dropdown>
+      {/* </Navbar.Brand> */}
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
